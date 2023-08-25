86c8525254963b1670c3857054bb5e0f69e5d736
Harold Murillo <harold.murillo@spasaude.org.br>
2023-08-25 11:35:50 -0300

    First commit

---
+++ src/main/java/br/com/hogwarts/artifactsonline/ArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.hogwarts.artifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactsOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hogwarts/artifactsonline/ArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hogwarts.artifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
